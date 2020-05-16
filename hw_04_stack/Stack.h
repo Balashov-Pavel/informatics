7f5a78ddf37d729b8e4f9c029d279c842b399f64
Balashov Pavel <64151493+Balashov-Pavel@users.noreply.github.com>
2020-05-16 14:13:47 +0300

    Create Stack.h

---
+++ hw_04_stack/Stack.h
@@ -0,0 +1,14 @@
+#ifndef STACK_H
+#define STACK_H
+
+typedef struct stack {
+	char data;
+	struct stack* sNext;
+}stack;
+
+input(struct stack** sHead, const char str);
+char pop(struct stack** sHead);
+is_empty(struct stack** sHead);
+check(const char* str);
+
+#endif
