7b94c481db3da4185b0eca5800f03767b2ea06ab
Balashov Pavel <64151493+Balashov-Pavel@users.noreply.github.com>
2020-05-24 20:08:34 +0300

    Update Source.c

--- hw_05_queue_verification/Source.c
+++ hw_05_queue_verification/Source.c
@@ -3,62 +3,59 @@
 #include <stdlib.h>
 #include "queue.h"
 
-function(int *arr, const int n)
+processing()
 {
-	int i, sum_1 = 0, sum_2 = 0;
+	int sum_1 = 0, sum_2 = 0, value;
 
 	struct queue queue;
 	queue.head = NULL;
 	queue.tail = NULL;
 
-	for (i = 0; i < n; i++)
-		enqueue(&queue, arr[i]);
+	scanf("%d", &value);
+	if (value == -1)
+	{
+		printf("Why are you here?");
+		return 0;
+	}
+	else 
+		while (value != -1)
+			{
+				enqueue(&queue, value);
+				scanf("%d", &value);
+			}
 
 	struct node *q = queue.head;
 
-	for (i = 0; i < n; i++)
+	while (q)
 	{
-		if (sum_1 == sum_2)
+		 if (sum_1 <= sum_2)
 		{
-			sum_1 += q->data;
-			q = q->next;
-			dequeue(&queue);
-
-			sum_2 += q->data;
-			q = q->next;
-			dequeue(&queue);
-			i++;
-		}
-		else if (sum_1 > sum_2)
-		{
-			sum_2 += q->data;
-			q = q->next;
-			dequeue(&queue);
+			 sum_1 += q->data;
+			 q = q->next;
+			 dequeue(&queue);
 		}
 		else
 		{
-			sum_1 += q->data;
+			sum_2 += q->data;
 			q = q->next;
 			dequeue(&queue);
 		}
 	}
 	if (sum_1 > sum_2)
+	{
 		printf("Result:%d\n", sum_1);
+		return 0;
+	}
 	else
+	{
 		printf("Result:%d\n", sum_2);
+		return 0;
+	}
 }
 
 int main()
 {
-	int n, i;
-	printf("Enter n:");
-	scanf("%d", &n);
-
-	int *arr = (int *)malloc(n * sizeof(int));
-	for (i = 0; i < n; i++)
-		scanf("%d", &arr[i]);
-	
-	function(arr, n);
+	processing();
 
 	while (getchar() != '\n');
 	getchar();
